3416fa3cfc674320ed4ffaaabf5fbc10276ecb15
talkgptworld <135692092+talkgptworld@users.noreply.github.com>
2023-08-14 15:47:03 +0000

    limit

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -41,6 +41,18 @@ export const post: APIRoute = async(context) => {
     initOptions.dispatcher = new ProxyAgent(httpsProxy)
   // #vercel-end
 
+  const resp1 = await fetch(`${import.meta.env.API_URL}/plugin/freesite/accesslog?ip=${context.clientAddress}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'GET',
+  })
+
+  const res = await resp1.text()
+  const resJson1 = JSON.parse(res)
+  if (resJson1.code !== 200)
+    return new Response(resJson1.message)
+
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
   const response = await fetch(`${baseUrl}/v1/chat/completions`, initOptions).catch((err: Error) => {
