4e543081d3a3aec0faed83377649dc840f617d33
ichatworld2023 <135700282+ichatworld2023@users.noreply.github.com>
2023-08-30 12:46:52 +0000

    change

--- src/components/Donation.tsx
+++ src/components/Donation.tsx
@@ -30,13 +30,19 @@ export default () => {
           <span class="border-1 px-2 py-1 mx-1 rounded-md transition-colors bg-slate/20 cursor-pointer hover:bg-slate/50">捐赠</span>
           <span>🙏🏻</span>
         </p>
+        <Show when={show() === 1}>
+          <div my-4>
+            <span op-60>使用微信扫一扫</span>
+            <img class="w-4/5 mt-2 max-w-[300px]" src="https://gzgptnb.oss-cn-guangzhou.aliyuncs.com/chatphp/202307/1690812533321.jpg" />
+          </div>
+        </Show>
         <p
           mt-1
           text-xs
           op-60
           w-full
           class={show() === 0 ? 'line-clamp-2' : ''}
-        >感谢捐赠 <span gpt-subtitle text-xs>匿名x44,菲小羊x5,Tifika非常想排练,192.168.0.1x2,Tim.ding,</span>lll,我推研磨天下第一,顾锦,Su_XH,大饼,MBKB,Su-27,Slug Cat,蔡晨威,Z,SfD,北欢南依,云追月,曹昕宇,.,蒋文彬,H,Pacino,锦帘,rumia,步欲不諪,小狗凯,XCalico,H,神不隆東,顺风车🍃,天南地北,系雾阁,久有凌云志,星月弥天,192.168.0.1,M,Accelerator,宥季,RoR,Ss🧸,Chrissie,Y,koil,samuraidog,心碎土豆丝,灼逝年华 阿康,.,海洋,龙王,凡死了.,LKJ,会游泳的鱼,七弦万木,随便你,随便你,路西法,蒂和平,🤐,长安.,学习,燚焱川,朱沐,麦芒,Joe,Melody,狐翎,方源,🍊,Wu Qiqian,天生猴子必有用,王越泽,天气日记,我的喵.cn,暖暖,一人一,Yuang,头绿心黄大菠萝OvO,菲小羊,吕刑褶,MYBEA***ASY,学习,姜*凡,E*s,二*三,SK**小明,Li**He,我被**包围了,你你你,雷,太阳保安,达不溜,Fe*ix,我叫孔子,neko**6z,巉巉,【那一段回忆、*】,缥缈,我在故我思,贰拾叁かい,zzzzy,Axis,腥白吆,C-c.减少期待,SD,Eurek4,哈哈,夏之星,chopsticks0253,I'm fine!,菲小羊,云,乏味,玖玖Rae
+        >感谢捐赠 <span gpt-subtitle text-xs>匿名x46,菲小羊x5,Tifika非常想排练,192.168.0.1x2,Tim.ding,</span>花开富贵🌸,凌乱_,難唸的經,lll,我推研磨天下第一,顾锦,Su_XH,大饼,MBKB,Su-27,Slug Cat,蔡晨威,Z,SfD,北欢南依,云追月,曹昕宇,.,蒋文彬,H,Pacino,锦帘,rumia,步欲不諪,小狗凯,XCalico,H,神不隆東,顺风车🍃,天南地北,系雾阁,久有凌云志,星月弥天,192.168.0.1,M,Accelerator,宥季,RoR,Ss🧸,Chrissie,Y,koil,samuraidog,心碎土豆丝,灼逝年华 阿康,.,海洋,龙王,凡死了.,LKJ,会游泳的鱼,七弦万木,随便你,随便你,路西法,蒂和平,🤐,长安.,学习,燚焱川,朱沐,麦芒,Joe,Melody,狐翎,方源,🍊,Wu Qiqian,天生猴子必有用,王越泽,天气日记,我的喵.cn,暖暖,一人一,Yuang,头绿心黄大菠萝OvO,菲小羊,吕刑褶,MYBEA***ASY,学习,姜*凡,E*s,二*三,SK**小明,Li**He,我被**包围了,你你你,雷,太阳保安,达不溜,Fe*ix,我叫孔子,neko**6z,巉巉,【那一段回忆、*】,缥缈,我在故我思,贰拾叁かい,zzzzy,Axis,腥白吆,C-c.减少期待,SD,Eurek4,哈哈,夏之星,chopsticks0253,I'm fine!,菲小羊,云,乏味,玖玖Rae
         </p>
         <p mt-2 class={show() === 0 ? 'hidden' : ''} gpt-subtitle text-xs>感谢B站UP主:叫憨子吧,木木子灵的推荐</p>
 
@@ -46,10 +52,6 @@ export default () => {
       </div>
       <Show when={show() === 1}>
         <div>
-          <div mt-4>
-            <span op-60>使用微信扫一扫</span>
-            <img class="w-4/5 mt-2 max-w-[300px]" src="https://gzgptnb.oss-cn-guangzhou.aliyuncs.com/chatphp/202307/1690812533321.jpg" />
-          </div>
 
           <div mt-2 op-60>
             感谢您的支持! <br />
