9bc2bded35475c37eb36a8f72df043921bb5ff49
usechatgptlive <135681258+usechatgptlive@users.noreply.github.com>
2023-10-17 11:02:33 +0000

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,18 @@ import solidJs from '@astrojs/solid-js'
 
 import node from '@astrojs/node'
 import AstroPWA from '@vite-pwa/astro'
-import vercel from '@astrojs/vercel/edge'
-import netlify from '@astrojs/netlify/edge-functions'
+import vercel from '@astrojs/vercel/serverless'
+import netlify from "@astrojs/netlify/functions"
 import disableBlocks from './plugins/disableBlocks'
 
 const envAdapter = () => {
   switch (process.env.OUTPUT) {
-    case 'vercel': return vercel()
-    case 'netlify': return netlify()
+    case 'vercel': return vercel({
+      edgeMiddleware: true
+    })
+    case 'netlify': return netlify({
+      edgeMiddleware: true
+    })
     default: return node({ mode: 'standalone' })
   }
 }
