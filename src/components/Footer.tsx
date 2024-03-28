fcd2d8c800419d0f6fb7facf634ad8a1aa5d5a0b
wanglihong996 <140092087+wanglihong996@users.noreply.github.com>
2024-03-28 06:08:23 +0000

    1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,6 +30,7 @@ export default () => {
     <footer>
       <div mt-6 text-xs>
         <div op-60>完全免费 无需魔法 无需登录 😎</div>
+        <div op-60>为了更好的满足大家的AI需求,本站点已升级为理解能力更强的gemini-pro 😎</div>
         <div mt-2 op-60>收藏不迷路: <a href="https://freegpts2.aifree.site" class="gpt-subtitle text-xs" target="_blank" rel="noreferrer">https://freegpts2.aifree.site</a></div>
         <div mt-2 op-60>中转地址: <span class="gpt-subtitle text-xs" >https://nav.aifree.site</span></div>
         <div mt-2><a target="_blank" class=" gpt-subtitle text-xs" href="https://chat11.aichats.site" rel="noreferrer">赞助商: 免费体验GPT4(已支持gpt-4-1106-preview)点击进入</a>😎</div>
