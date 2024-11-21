cd54e3e9ddb78fdd87448c3172da1d18c98394b5
wanglihong996 <140092087+wanglihong996@users.noreply.github.com>
2024-11-21 02:01:41 +0000

    1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,9 +33,9 @@ export default () => {
         <div op-60>为了更好的满足大家的AI需求,本站点已升级为理解能力更强的gemini-pro 😎</div>
         <div mt-2 op-60>收藏不迷路: <a href="https://freegpts2.aifree.site" class="gpt-subtitle text-xs" target="_blank" rel="noreferrer">https://freegpts2.aifree.site</a></div>
         <div mt-2 op-60>中转地址: <span class="gpt-subtitle text-xs" >https://nav.aifree.site</span></div>
-        <div mt-2><a target="_blank" class=" gpt-subtitle text-xs" href="https://chat11.ohmyaichat.online" rel="noreferrer">赞助商: 不够聪明?付费使用GPT4(已支持gpt-4o)点击进入</a>😎</div>
-        <div mt-2><a target="_blank" class=" gpt-subtitle text-xs" href="https://claude.ohmyaichat.online" rel="noreferrer">赞助商: 体验claude3(已支持claude-3-opus)点击进入</a>😎</div>
-        <div mt-2><a target="_blank" class=" gpt-subtitle text-xs" href="https://mini4.ohmyaichat.online" rel="noreferrer">赞助商: 比3.5更聪明,每问低至0.02元(最新gpt-4o-mini)点击进入</a>😎</div>
+        <div mt-2><a target="_blank" class=" gpt-subtitle text-xs" href="https://chat11.chatgg.world" rel="noreferrer">赞助商: 不够聪明?付费使用GPT4(已支持gpt-4o)点击进入</a>😎</div>
+        <div mt-2><a target="_blank" class=" gpt-subtitle text-xs" href="https://claude.chatgg.world" rel="noreferrer">赞助商: 体验claude3(已支持claude-3-opus)点击进入</a>😎</div>
+        <div mt-2><a target="_blank" class=" gpt-subtitle text-xs" href="https://mini4.chatgg.world" rel="noreferrer">赞助商: 比3.5更聪明,每问低至0.02元(最新gpt-4o-mini)点击进入</a>😎</div>
       </div>
       <div mt-6 text-xs>
         {/* <div op-60>当前在线人数:{online()}</div>
